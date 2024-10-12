fb6cf1f753b6f1f17231b144bd67537c8ecc5273
FacuCe <faqqu13@gmail.com>
2024-10-11 21:42:59 -0300

    google drive adapter

---
+++ lib/google_drive_adapter.rb
@@ -0,0 +1,15 @@
+module GoogleDriveAdapter
+  module_function
+
+  ROOT_PARENT_ID = '1ucIa7E9E3eG7ldf5ckqKZoVXTHf0qZcq'.freeze
+
+  def client
+    # TODO: las credenciales hay que guardarlas en otro lado
+    # doc: https://www.rubydoc.info/gems/google_drive/3.0.7/GoogleDrive/Session
+    @client ||= GoogleDrive::Session.from_config("lib/config.json")
+  end
+
+  def create_file(title, parent_id = ROOT_PARENT_ID)
+    client.create_file(title, parents: [parent_id])
+  end
+end
