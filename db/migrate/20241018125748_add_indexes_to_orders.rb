ffdffe9ad214c819f076980262acb08fedd97beb
Miguel Kruzliak <141929749+Miguelkzk@users.noreply.github.com>
2024-10-24 08:48:34 -0300

    Revert "fix in order detail"
    This reverts commit c61cbca7fbf6eacb8ad8c9822f44bbde413ffe71.

--- db/migrate/20241018125748_add_indexes_to_orders.rb
+++
@@ -1,9 +0,0 @@
-class AddIndexesToOrders < ActiveRecord::Migration[7.1]
-  def change
-    add_index :orders, :state
-    add_index :orders, :purchase_order
-    add_index :orders, :name
-
-    add_index :clients, :name
-  end
-end
